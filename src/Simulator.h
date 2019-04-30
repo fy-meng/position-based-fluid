af7aa6aa900beda66e50d1a0b676f1434ba5592b
Fanyu Meng <fanyu.meng98@gmail.com>
2019-04-29 22:04:34 -0700

    [minor] changed default steps per frame settings

--- src/Simulator.h
+++ src/Simulator.h
@@ -55,7 +55,7 @@ private:
   // Default simulation values
 
   int frames_per_sec = 90;
-  int simulation_steps = 30;
+  int simulation_steps = 1;
 
   CGL::Vector3D gravity = CGL::Vector3D(0, -9.8, 0);
   nanogui::Color color = nanogui::Color(1.0f, 1.0f, 1.0f, 1.0f);
